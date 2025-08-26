c3a88bb06a0d68a926b1148d1b5eec938e0aca1f
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2025-08-26 14:52:26 +0300

    ss

--- mining_platform/settings.py
+++ mining_platform/settings.py
@@ -216,3 +216,8 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Baghdad'  # غيّر بحسب منطقتك
+
+
+
+
+LOGIN_URL = '/dashboard/accounts/Login'
